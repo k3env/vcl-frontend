10f3a9e4e92a9305732068f04702b6f99a6f7b77
Sergey Tulonov <me@k3env.site>
2022-08-04 23:33:26 +0800

    EmployeeCard rewrite from class-based component to function component

--- src/components/EmployeeCard.tsx
+++ src/components/EmployeeCard.tsx
@@ -1,7 +1,8 @@
-import { Button, Card, Space, Text } from "@mantine/core";
-import React from "react";
+import { Button, Card, Group, Modal, Space, Text } from "@mantine/core";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import { Employee } from "../models/Employee";
+import { EmployeeAPI } from "../services/EmployeeAPI";
 
 interface EmployeeCardProps {
   canPress?: boolean;
@@ -9,66 +10,92 @@ interface EmployeeCardProps {
   employee: Employee;
 }
 
-export class EmployeeCard extends React.Component<EmployeeCardProps> {
-  private footer() {
-    if (!this.props.canPress) {
-      return (
-        <Card.Section p="xs">
-          <Button color="blue" component={Link} to="/">
-            Go back
-          </Button>
-          <Button
-            color="yellow"
-            component={Link}
-            to={`/employees/${this.props.employee.id}/edit`}
-            ml="xs"
-          >
-            Edit
-          </Button>
-        </Card.Section>
-      );
-    } else {
-      return (
-        <Card.Section p="xs">
-          <Button
-            color="green"
-            component={Link}
-            to={`/employees/${this.props.employee.id}`}
-          >
-            View
-          </Button>
-          <Button
-            color="yellow"
-            component={Link}
-            to={`/employees/${this.props.employee.id}/edit`}
-            ml="xs"
-          >
-            Edit
-          </Button>
-        </Card.Section>
-      );
-    }
-  }
-  render() {
+export function EmployeeCard(props: EmployeeCardProps) {
+  const [modalOpened, setModalOpened] = useState(false);
+  const footer = () => {
     return (
+      <Card.Section p="xs">
+        <Group spacing="xs">
+          {!props.canPress && (
+            <Button color="blue" component={Link} to="/">
+              Show all
+            </Button>
+          )}
+          {props.canPress && (
+            <Button
+              color="green"
+              component={Link}
+              to={`/employees/${props.employee.id}`}
+            >
+              View
+            </Button>
+          )}
+          <Button
+            color="yellow"
+            component={Link}
+            to={`/employees/${props.employee.id}/edit`}
+          >
+            Edit
+          </Button>
+          <Button color="red" onClick={() => setModalOpened(true)}>
+            Delete
+          </Button>
+          {!props.canPress && (
+            <Button
+              color="green"
+              component={Link}
+              to={`/employees/${props.employee.id}/vacation/new`}
+            >
+              Add vacation
+            </Button>
+          )}
+        </Group>
+      </Card.Section>
+    );
+  };
+  const handleEmployeeDelete = () => {
+    EmployeeAPI.delete(props.employee.id!).then(() => {
+      window.location.assign("/");
+    });
+  };
+  return (
+    <>
+      <Modal
+        opened={modalOpened}
+        onClose={() => setModalOpened(false)}
+        title={<Text size="xl">Are you sure?</Text>}
+        centered
+      >
+        <Text>Are you sure want delete {props.employee.name}?</Text>
+        <Text>This action is irreversible</Text>
+        <Space h="xl" />
+        <Group grow spacing="xs">
+          <Button color="red" onClick={handleEmployeeDelete}>
+            Yes
+          </Button>
+          <Button color="gray" onClick={() => setModalOpened(false)}>
+            No
+          </Button>
+        </Group>
+      </Modal>
       <Card withBorder shadow="sm" radius="md" p="xs" mb="xs">
         <Card.Section p="xs">
-          <Text size="xl">{this.props.employee.name}</Text>
+          <Text size="xl">{props.employee.name}</Text>
         </Card.Section>
         <Card.Section p="xs">
-          <Text>{`Created: ${this.props.employee.created_at?.toLocaleString()}`}</Text>
+          <Text>{`Created: ${props.employee.created_at?.toLocaleString()}`}</Text>
           <Space h="xs" />
-          <Text>{`Updated: ${this.props.employee.updated_at?.toLocaleString()}`}</Text>
+          <Text>{`Updated: ${props.employee.updated_at?.toLocaleString()}`}</Text>
           <Space h="xs" />
           <Text>
             Current color:{" "}
-            <Text component="span" style={{ color: this.props.employee.color }}>
-              {this.props.employee.color}
+            <Text component="span" style={{ color: props.employee.color }}>
+              {props.employee.color}
             </Text>
           </Text>
         </Card.Section>
-        {this.footer()}
+        {footer()}
       </Card>
-    );
-  }
+    </>
+  );
 }
