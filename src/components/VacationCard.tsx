acb6d71c3d5ab2df86a3f683987dfa56c803802f
Sergey Tulonov <me@k3env.site>
2022-08-09 19:46:42 +0800

    Перенес взаимодействие с API из VacationCard в VacationCardList

--- src/components/VacationCard.tsx
+++ src/components/VacationCard.tsx
@@ -1,75 +1,40 @@
 import {
   ActionIcon,
-  Button,
   Card,
   Group,
-  Modal,
-  Space,
   Text,
 } from "@mantine/core";
-import { showNotification } from "@mantine/notifications";
-import { IconCheck, IconPencil, IconX } from "@tabler/icons";
-import { useState } from "react";
+import { IconPencil, IconX } from "@tabler/icons";
 import { Link } from "react-router-dom";
 import { Vacation } from "../models/Vacation";
 
 type VacationProps = {
   vacation: Vacation;
-  onDelete?: () => void;
+  onDelete: (data?: any) => void;
 };
 
 export function VacationCard(props: VacationProps) {
   let endDate = props.vacation.start.plus({ days: props.vacation.length });
-  const [modalOpened, setModalOpened] = useState(false);
-  const handleVacationDelete = () => {
-    showNotification({
-      message: `x clicked on vacation #${JSON.stringify(props.vacation.id)}`,
-      color: "red",
-      title: "Deleting vacation",
-      icon: <IconCheck />,
-    });
-    setModalOpened(false);
-  };
   return (
-    <>
-      <Modal
-        opened={modalOpened}
-        onClose={() => setModalOpened(false)}
-        title={<Text size="xl">Are you sure?</Text>}
-        centered
-      >
-        <Text>Are you sure want delete vacation #{props.vacation.id}?</Text>
-        <Text>This action is irreversible</Text>
-        <Space h="xl" />
-        <Group grow spacing="xs">
-          <Button color="red" onClick={handleVacationDelete}>
-            Yes
-          </Button>
-          <Button color="gray" onClick={() => setModalOpened(false)}>
-            No
-          </Button>
+    <Card withBorder shadow="sm" radius="md" p="xs" mb="xs">
+      <Card.Section p="xs">
+        <Group position="right">
+          <ActionIcon
+            component={Link}
+            to={`/employees/${props.vacation.employee.id}/vacation/${props.vacation.id}/edit`}
+          >
+            <IconPencil />
+          </ActionIcon>
+          <ActionIcon onClick={() => props.onDelete(props.vacation.id)}>
+            <IconX />
+          </ActionIcon>
         </Group>
-      </Modal>
-      <Card withBorder shadow="sm" radius="md" p="xs" mb="xs">
-        <Card.Section p="xs">
-          <Group position="right">
-            <ActionIcon
-              component={Link}
-              to={`/employees/${props.vacation.employee.id}/vacation/${props.vacation.id}/edit`}
-            >
-              <IconPencil />
-            </ActionIcon>
-            <ActionIcon onClick={() => setModalOpened(true)}>
-              <IconX />
-            </ActionIcon>
-          </Group>
-        </Card.Section>
-        <Card.Section p="xs">
-          <Text>From: {props.vacation.start.toLocaleString()}</Text>
-          <Text>Length: {props.vacation.length} days</Text>
-          <Text>Due date: {endDate.toLocaleString()}</Text>
-        </Card.Section>
-      </Card>
-    </>
+      </Card.Section>
+      <Card.Section p="xs">
+        <Text>From: {props.vacation.start.toLocaleString()}</Text>
+        <Text>Length: {props.vacation.length} days</Text>
+        <Text>Due date: {endDate.toLocaleString()}</Text>
+      </Card.Section>
+    </Card>
   );
 }
--- src/components/VacationCardList.tsx
+++ src/components/VacationCardList.tsx
@@ -1,27 +1,93 @@
-import { SimpleGrid } from "@mantine/core";
+import { Alert, Button, Group, Modal, SimpleGrid, Space, Text } from "@mantine/core";
+import { showNotification } from "@mantine/notifications";
+import { IconAlertCircle } from "@tabler/icons";
+import { AxiosError } from "axios";
 import { useEffect, useState } from "react";
 import { TEmployeeSingle } from "../models/Employee";
 import { Vacation } from "../models/Vacation";
-import { VacationAPI } from "../services/VacationAPI";
+import { DeleteVacationResponse, VacationAPI } from "../services/VacationAPI";
 import { LoadingScreen } from "./LoadingScreen";
 import { VacationCard } from "./VacationCard";
 
 export function VacationCardList(props: TEmployeeSingle) {
   const [state, setState] = useState<Vacation[] | null>(null);
+  const [count, setCount] = useState<number>(0)
+  const [vID, setVID] = useState<number | undefined>()
   useEffect(() => {
     const handleVacationsLoad = (vs: Vacation[]) => setState(vs);
     VacationAPI.list(props.employee?.id ?? 0).then(handleVacationsLoad);
-  }, [props.employee?.id]);
+  }, [props.employee?.id, count]);
+
+  const [modalOpened, setModalOpened] = useState(false);
+  const [onLoading, setOnLoading] = useState(false);
+  const [childItem, setChildItem] = useState((<></>));
 
   if (state === null) {
     return <LoadingScreen />;
   }
 
+  const handleVacationDelete = () => {
+    if (vID !== undefined) {
+      setOnLoading(true);
+      VacationAPI.delete(vID)
+        .then((data) => {
+          setOnLoading(false);
+          setModalOpened(false);
+          handleDeleteSuccess(data)
+        }, (reason: AxiosError) => {
+          setOnLoading(false);
+          setChildItem((<Alert icon={<IconAlertCircle size={16} />} title="Something went wrong" color="red">
+            Something terrible happened: {JSON.stringify(reason.response?.data)}
+          </Alert>))
+        });
+    }
+  };
+
+  const handleDeleteSuccess = (data: DeleteVacationResponse) => {
+    setCount(count + 1);
+    showNotification({
+      color: 'green',
+      title: 'Vacation deleted',
+      message: `Vacation #${data.id} deleted`
+    })
+  }
+
+  const handleDeleteClick = (id: number) => {
+    setVID(id);
+    setModalOpened(true)
+  }
+
   return (
-    <SimpleGrid cols={4} spacing="xs">
-      {state?.map((v) => (
-        <VacationCard vacation={v} key={v.id?.toString()} />
-      ))}
-    </SimpleGrid>
+    <>
+      <Modal
+        opened={modalOpened}
+        onClose={() => setModalOpened(false)}
+        title={<Text size="xl">Are you sure?</Text>}
+        centered
+      >
+        <Text>Are you sure want delete vacation #{vID}?</Text>
+        <Text>This action is irreversible</Text>
+        <Space h="xl" />
+        {childItem}
+        <Space h="xl" />
+        <Group grow spacing="xs">
+          <Button
+            color="red"
+            onClick={handleVacationDelete}
+            loading={onLoading}
+          >
+            Yes
+          </Button>
+          <Button color="gray" onClick={() => setModalOpened(false)}>
+            No
+          </Button>
+        </Group>
+      </Modal>
+      <SimpleGrid cols={4} spacing="xs">
+        {state?.map((v) => (
+          <VacationCard vacation={v} key={v.id?.toString()} onDelete={handleDeleteClick} />
+        ))}
+      </SimpleGrid>
+    </>
   );
 }
