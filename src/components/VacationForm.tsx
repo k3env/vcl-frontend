deb039ca9249a82eb9cf47f1693c58a9ba9f4b57
Sergey Tulonov <me@k3env.site>
2022-08-09 20:17:50 +0800

    Vacation form * Link for 'Go back' button fix * Added missing `required` prop to employee select * Fixed placeholder for vacation length input * changed form width from 300px to 500px * Added error handlers to post/patch requests. * Changed component state type - added flag for loading state instead comparing vacation and employees with null * Form initial values fix

--- src/components/VacationForm.tsx
+++ src/components/VacationForm.tsx
@@ -3,16 +3,20 @@ import { useForm } from "@mantine/form";
 import { DatePicker } from "@mantine/dates";
 import { DateTime } from "luxon";
 import { useEffect, useState } from "react";
-import { Link, useParams } from "react-router-dom";
+import { Link, useParams, useNavigate } from "react-router-dom";
 import { Employee } from "../models/Employee";
-import { Vacation } from "../models/Vacation";
+import { Vacation, IVacationFormData } from "../models/Vacation";
 import { VacationAPI } from "../services/VacationAPI";
 import { EmployeeAPI } from "../services/EmployeeAPI";
 import { LoadingScreen } from "./LoadingScreen";
+import { showNotification } from "@mantine/notifications";
+import { IconX } from "@tabler/icons";
+import { AxiosError } from "axios";
 
 type VacationFormState = {
-  vacation: Vacation | null;
-  employees: EmployeeSelectData[] | null;
+  vacation?: Vacation;
+  employees?: EmployeeSelectData[];
+  loading: boolean;
 };
 
 type EmployeeSelectData = {
@@ -22,86 +26,90 @@ type EmployeeSelectData = {
 
 export function VacationForm(props: {}) {
   const [state, setState] = useState<VacationFormState>({
-    vacation: null,
-    employees: null,
+    loading: true,
+    vacation: undefined,
+    employees: undefined
   });
-  const form = useForm({
+  const nav = useNavigate();
+  let params = useParams();
+  const form = useForm<IVacationFormData>({
     initialValues: {
-      start: state.vacation?.start.toJSDate() ?? DateTime.now().toJSDate(),
-      length: state.vacation?.length ?? 3,
-      employee: (state.vacation?.employee.id ?? 0).toString(),
+      start: DateTime.now().toJSDate(),
+      length: 3,
+      employee: params.employee_id ?? '0',
     },
   });
-  let params = useParams();
+
+
   useEffect(() => {
     const handleVacationLoad = (v: Vacation) => {
-      const vObj = { vacation: v };
-      setState((ov) => {
-        return { ...ov, ...vObj };
-      });
+      setState((os) => { return { ...os, ...{ vacation: v, loading: false } } })
       form.setValues({
         start: v.start.toJSDate(),
         length: v.length,
-        employee: (v.employee.id ?? 0).toString(),
+        employee: (params.employee_id ?? 0).toString(),
       });
     };
+
+    const handleError = () => {
+      setState((os) => { return { ...os, ...{ loading: false } } });
+      form.setValues({
+        start: new Date(),
+        length: 3,
+        employee: (params.employee_id ?? 0).toString(),
+      });
+    }
     const handleEmployeesLoad = (es: Employee[]) => {
-      const eObj = {
-        employees: es.map<EmployeeSelectData>((e) => {
-          return { value: (e.id ?? 0).toString(), label: e.name };
-        }),
-      };
-      setState((ov) => {
-        return { ...ov, ...eObj };
-      });
+      const ems = es.map<EmployeeSelectData>((e) => {
+        return { value: (e.id ?? 0).toString(), label: e.name };
+      })
+      setState((os) => { return { ...os, ...{ employees: ems, loading: true } } });
+
+      const vID = Number.parseInt(params.vID ?? "0", 10);
+      if (vID !== 0) {
+        VacationAPI.get(vID).then(handleVacationLoad, handleError);
+      } else {
+        handleError()
+      }
     };
-    const vID = Number.parseInt(params.vID ?? "0", 10);
-    VacationAPI.get(vID).then(handleVacationLoad);
-    EmployeeAPI.list().then(handleEmployeesLoad);
+    EmployeeAPI.list().then(handleEmployeesLoad)
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [params.vID]);
-  if (state.vacation === null || state.employees === null) {
+  if (state.loading) {
     return <LoadingScreen />;
   }
-  const formOnSubmit = async (v: {
-    start: Date;
-    length: number;
-    employee: string;
-  }) => {
-    // TODO: Добавить обработчик добавления нового отпуска сотруднику.
-    let vacation = state.vacation;
-    if (vacation !== null && (vacation.id !== 0 || vacation.id !== undefined)) {
-      vacation.start = DateTime.fromJSDate(v.start);
-      vacation.length = v.length;
-      vacation.employee = await EmployeeAPI.get(
-        Number.parseInt(v.employee, 10)
-      );
-    } else {
-      vacation = new Vacation(
-        undefined,
-        DateTime.fromJSDate(v.start).toISO(),
-        v.length,
-        await EmployeeAPI.get(Number.parseInt(v.employee, 10))
-      );
-    }
-
-    const vpl = vacation.toPayload();
-    console.log(vpl);
+  const formOnSubmit = async (v: IVacationFormData) => {
+    const handleSuccessRequest = () => nav(`/employees/${v.employee}`);
+    const handleError = (reason: AxiosError) => {
+      showNotification({
+        title: `Error: ${reason.code}`,
+        message: `${reason.response?.statusText
+          }: ${reason.response?.config.method?.toUpperCase()} ${reason.request.responseURL
+          }`,
+        color: "red",
+        icon: <IconX />,
+      });
+    };
+    let vacation = Vacation.formDataToPayload(v);
     if (params.vID === undefined) {
-      VacationAPI.post(vacation.employee.id!, vpl).then((v) =>
-        window.location.assign(`/employees/${params.employee_id}`)
+      VacationAPI.post(vacation.employee_id, vacation).then(
+        handleSuccessRequest,
+        handleError
       );
     } else {
-      VacationAPI.patch(vacation.id!, vpl).then((v) =>
-        window.location.assign(`/employees/${params.employee_id}`)
+      VacationAPI.patch(Number.parseInt(params.vID, 10), vacation).then(
+        handleSuccessRequest,
+        handleError
       );
     }
   };
+
+  // Markup here
   return (
-    <Box sx={{ maxWidth: 300 }} mx="auto">
+    <Box sx={{ maxWidth: 500 }} mx="auto">
       <form onSubmit={form.onSubmit((v) => formOnSubmit(v))}>
         <Text size="xl">
-          {params.vID
+          {state.vacation
             ? `Editing vacation #${state.vacation.id}`
             : "Add vacation"}
         </Text>
@@ -112,7 +120,7 @@ export function VacationForm(props: {}) {
           {...form.getInputProps("start")}
         />
         <NumberInput
-          placeholder="Your age"
+          placeholder="Vacation length"
           label="Vacation length"
           required
           {...form.getInputProps("length")}
@@ -120,13 +128,18 @@ export function VacationForm(props: {}) {
         <Select
           data={state.employees}
           label="Select employee"
+          required
           {...form.getInputProps("employee")}
         />
         <Group position="right" mt="md">
           <Button type="submit" color="green">
             Submit
           </Button>
-          <Button color="yellow" component={Link} to="/">
+          <Button
+            color="yellow"
+            component={Link}
+            to={`/employees/${params.employee_id}`}
+          >
             Go back
           </Button>
         </Group>
