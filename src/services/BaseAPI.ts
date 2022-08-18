a71f95825171e911bd7ad742285d1de28656c2b5
Sergey Tulonov <me@k3env.site>
2022-08-18 17:47:12 +0800

    Добавил типизацию апи клиенту

--- src/services/BaseAPI.ts
+++ src/services/BaseAPI.ts
@@ -1,6 +1,8 @@
-import axios from "axios";
+import axios, { AxiosInstance } from "axios";
 export class BaseAPI {
   private static BASE_URL = "https://vcl-be.herokuapp.com/api";
   // private static BASE_URL = "http://localhost:3333/api";
-  protected static client = axios.create({ baseURL: this.BASE_URL });
+  protected static client: AxiosInstance = axios.create({
+    baseURL: this.BASE_URL,
+  });
 }
