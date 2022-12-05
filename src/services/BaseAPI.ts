dc0605d4c873b21eccd3144dfd0e36ec9916ecf3
Sergey Tulonov <me@k3env.site>
2022-12-05 10:21:17 +0800

    Update BaseAPI.ts
    Update API URL

--- src/services/BaseAPI.ts
+++ src/services/BaseAPI.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosInstance } from "axios";
 export class BaseAPI {
-  private static BASE_URL = "https://vcl-be.herokuapp.com/api";
+  private static BASE_URL = "https://vcl-backend-production.up.railway.app/api";
   // private static BASE_URL = "http://localhost:3333/api";
   protected static client: AxiosInstance = axios.create({
     baseURL: this.BASE_URL,
